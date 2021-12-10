dd98a34ce057a614e7ed004b5d4f25a04e5321ac
PFiS1737 <3165823482@qq.com>
2021-12-10 09:12:49 +0800

    更新 build.js

--- build.js
+++ build.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 const path = require('path')
 
-copyDir("./node_modules/mdui", "./public/mdui")
+copyDir("./node_modules/mdui", "./public/mdui", [ "es", "src" ])
 copyDir("./node_modules/eruda", "./public/eruda")
 
 function copyFile(from, to) {
@@ -11,13 +11,14 @@ function copyFile(from, to) {
     if (!fs.existsSync(to)) fs.mkdirSync(to)
     fs.copyFileSync(fromPath, toPath + "/" + fileName)
 }
-function copyDir(from, to) {
+function copyDir(from, to, ignore = []) {
     let fromPath = path.resolve(from)
     let toPath = path.resolve(to)
     if (!fs.existsSync(to)) fs.mkdirSync(to)
     fs.readdir(fromPath, (err, paths) => {
         if (err) return console.error(err)
         paths.forEach(item => {
+            if (ignore.includes(item)) return
             let newFromPath = fromPath + '/' + item
             let newToPath = path.resolve(toPath + '/' + item)
             fs.stat(newFromPath, (err, stat) => {
